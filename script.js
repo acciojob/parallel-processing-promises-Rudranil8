df2b3968424cfdca793cd98f5675bcea25c82af4
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 17:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,22 +8,55 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-btn.addEventListener("click", () => {
-    let promises = images.map(image => loadImage(image.url));
-    Promise.all(promises)
-        .then(imgs => {
-            imgs.forEach(img => {
-                output.appendChild(img);
-            });
-        })
-        .catch(error => console.error(error));
+// btn.addEventListener("click", () => {
+//     let promises = images.map(image => loadImage(image.url));
+//     Promise.all(promises)
+//         .then(imgs => {
+//             imgs.forEach(img => {
+//                 output.appendChild(img);
+//             });
+//         })
+//         .catch(error => console.error(error));
+// });
+
+// function loadImage(url) {
+//     return new Promise((resolve, reject) => {
+//         let img = new Image();
+//         img.src = url;
+//         img.onload = () => resolve(img);
+//         img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
+//     });
+// }
+
+window.onload = function() {
+  const output = document.getElementById("output");
+  const btn = document.getElementById("download-images-button");
+	const output = document.getElementById(“output”);
+const btn = document.getElementById(“download-images-button”);
+
+const images = [
+{ url: “https://picsum.photos/id/237/200/300” },
+{ url: “https://picsum.photos/id/238/200/300” },
+{ url: “https://picsum.photos/id/239/200/300” },
+];
+
+btn.addEventListener(“click”, () => {
+let promises = images.map(image => loadImage(image.url));
+Promise.all(promises)
+.then(imgs => {
+imgs.forEach(img => {
+output.appendChild(img);
+});
+})
+.catch(error => console.error(error));
 });
 
 function loadImage(url) {
-    return new Promise((resolve, reject) => {
-        let img = new Image();
-        img.src = url;
-        img.onload = () => resolve(img);
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
-    });
+return new Promise((resolve, reject) => {
+let img = new Image();
+img.src = url;
+img.onload = () => resolve(img);
+img.onerror = () => reject(new Error(Failed to load image's URL: ${url}));
+});
+}
 }
