41437338a2f2a97bb9dea67fff1b15bfde075a5f
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 17:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+    let promises = images.map(image => loadImage(image.url));
+    Promise.all(promises)
+        .then(imgs => {
+            imgs.forEach(img => {
+                output.appendChild(img);
+            });
+        })
+        .catch(error => console.error(error));
+});
