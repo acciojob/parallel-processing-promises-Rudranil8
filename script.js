4c44e69246b34ed19547d082ccc3fd0cce7634db
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 17:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ btn.addEventListener("click", () => {
         })
         .catch(error => console.error(error));
 });
+
+function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.src = url;
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
+    });
+}
